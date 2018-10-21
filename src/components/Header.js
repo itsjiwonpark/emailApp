f3e910cf2a6e12ed4258ca85dae42ad5eeacc181
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro.local>
2018-10-21 17:54:07 +0900

    api 연결한거까지만.. 오아쓰 하기 전ㅠㅠ

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,50 @@ import axios from "axios";
 // import "../style.scss";
 
 class Header extends Component {
-  // componentDidMount() {
-  //   const url = `https://sheets.googleapis.com/v4/spreadsheets/1boLea3iXMGz4E_8Anh6_00J8OllMRMIFZHhpUr5d8qY/values/'시트1'!A1:A5?majorDimension=COLUMNS`;
-  //   axios
-  //     .get(url)
-  //     .then(res => {
-  //       console.log(res);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }
+  state = {
+    email: ""
+  };
+  componentDidMount() {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/1boLea3iXMGz4E_8Anh6_00J8OllMRMIFZHhpUr5d8qY/values/시트1!A1:A5?key=AIzaSyCzr9-9G-HzdTZWIjirNuROIY9OcNSQ0zA&majorDimension=COLUMNS`;
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data.values[0], "짱짱짱 엄청나");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  _putEmailAddress = e => {
+    const email = e.target.value;
+    this.setState({ email });
+  };
+
+  _appendToSheet = () => {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/1boLea3iXMGz4E_8Anh6_00J8OllMRMIFZHhpUr5d8qY/values/시트1!A1:append?key=AIzaSyCzr9-9G-HzdTZWIjirNuROIY9OcNSQ0zA&valueInputOption=USER_ENTERED`;
+    const email = this.state.email;
+    axios
+      .post(url, {
+        range: "시트1!A1",
+        majorDimension: "ROWS",
+        values: [[email]],
+        apiKey: "AIzaSyCzr9-9G-HzdTZWIjirNuROIY9OcNSQ0zA"
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>I'm header</h1>
+        <input type="email" onChange={this._putEmailAddress} />
+        <input type="button" onClick={this._appendToSheet} />
       </div>
     );
   }
