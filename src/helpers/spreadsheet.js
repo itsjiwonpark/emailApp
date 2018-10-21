1f9a9ec89ad94831ed8c98a4720dd46fbb1a7987
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro.local>
2018-10-21 19:48:11 +0900

    OAuth 해결중..

--- src/helpers/spreadsheet.js
+++ src/helpers/spreadsheet.js
@@ -5,28 +5,49 @@ export function load(callback, data) {
   console.log(range);
   window.gapi.client.load("sheets", "v4", () => {
     window.gapi.client.sheets.spreadsheets.values
-      .get({
+      .append({
         spreadsheetId: config.spreadsheetId,
-        range: "시트1!A1:A5",
-        majorDimension: "COLUMNS"
+        range: "시트1!A1",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          value: [["hi"]]
+        }
       })
       .then(
         response => {
-          const emailLists = response.result.values[0];
-          // const cars =
-          //   data.map(car => ({
-          //     year: car[0],
-          //     make: car[1],
-          //     model: car[2]
-          //   })) || [];
-          callback({
-            emailLists
-          });
+          var result = response.result;
+          console.log(`${result.updates.updatedCells} cells appended.`);
+          callback(result);
         },
         response => {
           callback(false, response.result.error);
+          console.log(response.result.error);
         }
       );
+
+    // window.gapi.client.sheets.spreadsheets.values
+    //   .get({
+    //     spreadsheetId: config.spreadsheetId,
+    //     range: "시트1!A1:A5",
+    //     majorDimension: "COLUMNS"
+    //   })
+    //   .then(
+    //     response => {
+    //       const emailLists = response.result.values[0];
+    //       // const cars =
+    //       //   data.map(car => ({
+    //       //     year: car[0],
+    //       //     make: car[1],
+    //       //     model: car[2]
+    //       //   })) || [];
+    //       callback({
+    //         emailLists
+    //       });
+    //     },
+    //     response => {
+    //       callback(false, response.result.error);
+    //     }
+    //   );
   });
 }
 
